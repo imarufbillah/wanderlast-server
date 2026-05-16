cfbffe5c1b82f4b9f063365678525c9a9563fbc3
Maruf Billah <marufbillah.dev@gmail.com>
2026-05-16 16:10:10 +0600

    feat: disable MongoDB connection and ping for local testing

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const verifyAdminJWT = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("wanderlast");
     const destinationsCollection = database.collection("destinations");
@@ -209,7 +209,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
